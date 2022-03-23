83fa9c0d45ee16fd7a1bf510f084e836f6159571
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-03-23 20:48:08 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -88,16 +88,16 @@ async def button(bot, update):
           return
         await update.message.delete(True)
         await bot.send_message(
+            chat_id=update.message.chat.id,
+            text="now send me a new name for the file",
+            reply_to_message_id=updete.message.reply_to_message.message_id,
+            reply_markup=ForceReply(False)
+        )
      
      
-              chat_id=update.message.chat.id,
-              text="now send me a new name for the file",
-              reply_to_message_id=updete.message.reply_to_message.message_id,
-              reply_markup=ForceReply(False)
-              )
+              
      
-        
-        if (" " in update.text) and (update.reply_to_message is not None):
+
         
         
                                                                         
